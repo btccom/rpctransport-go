e972f7035da0b2069472b79d070a30913948bf3c
Thomas Kerin <thomas.kerin@bitmaintech.com>
2018-02-03 19:30:05 +0100

    Adds DummyClient

---
+++ dummyrpc/client.go
@@ -0,0 +1,61 @@
+package dummyrpc
+
+import (
+	"sync"
+	"github.com/btccom/rpctransport-go/rpc"
+)
+
+func NewDummyClient(server *DummyServer) rpc.Client {
+	return &DummyClient{
+		pending: make(map[string]*pendingRequest),
+		server: server,
+	}
+}
+
+type pendingRequest struct {
+	request []byte
+	resultChan chan []byte
+	errorChan  chan error
+}
+
+func newPendingRequest(request []byte) *pendingRequest {
+	return &pendingRequest{
+		request: request,
+		resultChan: make(chan []byte),
+		errorChan:  make(chan error),
+	}
+}
+
+type DummyClient struct {
+	pending map[string]*pendingRequest
+	server *DummyServer
+	sendLock sync.RWMutex
+}
+
+func (ad *DummyClient) Close() error {
+	return nil
+}
+
+func (ad *DummyClient) Dial() error {
+	return nil
+}
+
+func (ad *DummyClient) RequestAsync(request []byte) (<-chan []byte, <-chan error) {
+	pending := newPendingRequest(request)
+
+	ad.server.In <- pending
+
+	return pending.resultChan, pending.errorChan
+}
+
+func (ad *DummyClient) Request(request []byte) ([]byte, error) {
+	resultChan, errorChan := ad.RequestAsync(request)
+
+	select {
+	case result := <-resultChan:
+		return result, nil
+	case err := <-errorChan:
+		return nil, err
+	}
+}
+
